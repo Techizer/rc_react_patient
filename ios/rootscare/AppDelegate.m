11d7b66de6144379b0659ba397b9dc4dee7e257d
Ahsan <mobile.developers02@gmail.com>
2023-02-08 10:53:49 +0500

    Latest Code

--- ios/rootscare/AppDelegate.m
+++ ios/rootscare/AppDelegate.m
@@ -1,3 +1,5 @@
+
+#import <RNBranch/RNBranch.h>
 #import <UserNotifications/UserNotifications.h>
 #import <RNCPushNotificationIOS.h>
 #import "AppDelegate.h"
@@ -6,7 +8,7 @@
 #import <React/RCTRootView.h>
 #import <React/RCTI18nUtil.h>
 #import <Firebase.h>
-
+#import <React/RCTLinkingManager.h> 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
@@ -39,10 +41,20 @@ static void InitializeFlipper(UIApplication *application) {
   InitializeFlipper(application);
 #endif
      [FIRApp configure];
-  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+    RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
    [[RCTI18nUtil sharedInstance] allowRTL:YES];
    [[RCTI18nUtil sharedInstance] forceRTL:YES];
+ 
+[[FBSDKApplicationDelegate sharedInstance] application:application
+didFinishLaunchingWithOptions:launchOptions];
 
+  [[Branch getInstance] initSessionWithLaunchOptions:launchOptions andRegisterDeepLinkHandler:^(NSDictionary * _Nonnull params, NSError * _Nullable error) {
+    // do stuff with deep link data (nav to page, display content, etc)
+    NSLog(@"%@", params);
+  }];
+  
+  
+  
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"rootscare"
                                             initialProperties:nil];
@@ -61,6 +73,20 @@ UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotification
   [self.window makeKeyAndVisible];
   return YES;
 }
+
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+  [[Branch getInstance] application:app openURL:url options:options];
+  if ([[FBSDKApplicationDelegate sharedInstance] application:app openURL:url options:options]) {
+    return YES;
+  }
+
+  if ([RCTLinkingManager application:app openURL:url options:options]) {
+    return YES;
+  }
+
+  return NO;
+}
+
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
 {
  [RNCPushNotificationIOS didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
@@ -70,7 +96,15 @@ UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotification
 fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
 {
   [RNCPushNotificationIOS didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
+  [[Branch getInstance] handlePushNotification:userInfo];
 }
+
+- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray * _Nullable))restorationHandler {
+  // handler for Universal Links
+  [[Branch getInstance] continueUserActivity:userActivity];
+  return YES;
+}
+
 // Required for the registrationError event.
 - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
 {
---
+++ ios/rootscare/rootscareApp.swift
@@ -0,0 +1,17 @@
+//
+//  rootscareApp.swift
+//  rootscare
+//
+//  Created by Ahsan Farooq on 06/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct rootscareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
